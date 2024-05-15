68aa3ad90c771ed4090fb015fa06a175485e5a46
Himanshu Goyal <hggoyal06@gmail.com>
2024-05-16 01:45:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,130 @@
+import streamlit as st
+import json
+import pandas as pd
+import time
+import zipfile
+
+#####################
+#### VERSION 1.0 ####
+#####################
+
+
+################################################################################
+
+
+
+st.set_page_config(page_title="Process File", page_icon=":bar_chart:")
+# st.image("Logo.png", caption=None, width=250, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+
+
+def add_filter():
+    st.session_state.num_filter += 1
+
+
+def generate_files():
+    df = st.session_state.df
+    num_filter = st.session_state.num_filter
+
+
+    i = 1
+    while i <= num_filter:
+
+        filters = st.session_state[f"File-{i}"]
+        filename = st.session_state[f"Filename-{i}"]
+        if filename.endswith(".xlsx"):
+            pass
+        else:
+            filename = filename + ".xlsx"
+
+        sub_df = df[df["State-County"].isin(filters)]
+        sub_df = sub_df.drop("State-County", axis = 1)
+        sub_df.to_excel(filename, index = False)
+
+        i += 1
+
+
+    _zip = zipfile.ZipFile("Processed.zip", "w", zipfile.ZIP_DEFLATED)
+
+    i = 1
+    while i <= num_filter:
+
+        filename = st.session_state[f"Filename-{i}"]
+        if filename.endswith(".xlsx"):
+            pass
+        else:
+            filename = filename + ".xlsx"
+        _zip.write(filename)
+
+        i += 1
+
+    _zip.close()    
+
+    with open("Processed.zip", "rb") as zipped:
+        encoded_string = zipped.read()
+
+    st.session_state.zipped = encoded_string
+
+
+
+
+if "num_filter" not in st.session_state:
+    st.session_state.num_filter = 1
+
+if "df" not in st.session_state:
+    df = pd.DataFrame([],columns=["State-County"])
+
+    st.session_state.df = df
+    st.session_state.filters = list(df["State-County"].unique())
+
+
+
+uploaded_file = st.file_uploader("Choose a .csv/.xlsx file", type = ["csv", "xlsx"])
+
+
+if uploaded_file is not None:
+
+    df = pd.read_excel(uploaded_file)
+
+    cols_required = ["Filing_County", "Filing_State"] 
+    cols_not_found = []
+    for col in cols_required:
+        if col not in df.columns:
+            cols_not_found.append(col)
+
+    if len(cols_not_found) > 0:
+        errors = {"Columns Not Found" : cols_not_found}
+
+        st.error(json.dumps(errors, indent = 3)) 
+
+
+    else:
+        df["State-County"] = df["Filing_County"] + "-" + df["Filing_State"]
+
+        st.session_state.df = df
+        st.session_state.filters = list(df["State-County"].unique())
+
+
+        add_row = st.sidebar.button("Add Filter", on_click = add_filter)
+
+        i = 1
+
+        while i <= st.session_state.num_filter:
+
+            st.sidebar.multiselect(f"File {i}", options = st.session_state.filters, default = [], key = f"File-{i}")
+            st.sidebar.text_input("", value = f"File-{i}.xlsx", placeholder = f"File {i}.xlsx" , label_visibility="collapsed", key = f"Filename-{i}")
+
+
+            i = i + 1
+
+
+        zipped_file = st.sidebar.button("Process Files", key='but_generate', on_click = generate_files)
+
+        if zipped_file:
+            download = st.sidebar.download_button(label="Download Files", data = st.session_state.zipped, file_name= "Processed.zip", mime='application/octet-stream')
+
+
+        st.write("Data Preview")
+        st.write(df.drop("State-County", axis = 1))
+
+

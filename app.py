fc45b127a6ca5c5c3d5541d4a75cf160a29ab828
Himanshu Goyal <hggoyal06@gmail.com>
2024-05-25 12:47:42 +0530

    Test Changes app.py

--- app.py
+++ app.py
@@ -132,45 +132,45 @@ if uploaded_file is not None:
     # df = pd.read_excel(uploaded_file)
     df = read_excel(uploaded_file)
 
-    cols_required = ["Filing_County", "Filing_State"] 
-    cols_not_found = []
-    for col in cols_required:
-        if col not in df.columns:
-            cols_not_found.append(col)
+    # cols_required = ["Filing_County", "Filing_State"] 
+    # cols_not_found = []
+    # for col in cols_required:
+    #     if col not in df.columns:
+    #         cols_not_found.append(col)
 
-    if len(cols_not_found) > 0:
-        errors = {"Columns Not Found" : cols_not_found}
+    # if len(cols_not_found) > 0:
+    #     errors = {"Columns Not Found" : cols_not_found}
 
-        st.error(json.dumps(errors, indent = 3)) 
-
-
-    else:
-        # df["State-County"] = df["Filing_County"] + "-" + df["Filing_State"]
-
-        st.session_state.df = df
-        st.session_state.filters = list(df["State-County"].unique())
+    #     st.error(json.dumps(errors, indent = 3)) 
 
 
-        add_row = st.sidebar.button("Add Filter", on_click = add_filter)
+    # else:
+    #     # df["State-County"] = df["Filing_County"] + "-" + df["Filing_State"]
 
-        i = 1
-
-        while i <= st.session_state.num_filter:
-
-            st.sidebar.multiselect(f"File {i}", options = st.session_state.filters, default = [], key = f"File-{i}")
-            st.sidebar.text_input("Random", value = f"File-{i}.xlsx", placeholder = f"File {i}.xlsx" , label_visibility="collapsed", key = f"Filename-{i}")
+    #     st.session_state.df = df
+    #     st.session_state.filters = list(df["State-County"].unique())
 
 
-            i = i + 1
+    #     add_row = st.sidebar.button("Add Filter", on_click = add_filter)
+
+    #     i = 1
+
+    #     while i <= st.session_state.num_filter:
+
+    #         st.sidebar.multiselect(f"File {i}", options = st.session_state.filters, default = [], key = f"File-{i}")
+    #         st.sidebar.text_input("Random", value = f"File-{i}.xlsx", placeholder = f"File {i}.xlsx" , label_visibility="collapsed", key = f"Filename-{i}")
 
 
-        zipped_file = st.sidebar.button("Process Files", key='but_generate', on_click = generate_files)
-
-        if zipped_file:
-            download = st.sidebar.download_button(label="Download Files", data = st.session_state.zipped, file_name= "Processed.zip", mime='application/octet-stream')
+    #         i = i + 1
 
 
-        st.write("Data Preview")
-        st.write(df.drop("State-County", axis = 1))
+    #     zipped_file = st.sidebar.button("Process Files", key='but_generate', on_click = generate_files)
+
+    #     if zipped_file:
+    #         download = st.sidebar.download_button(label="Download Files", data = st.session_state.zipped, file_name= "Processed.zip", mime='application/octet-stream')
+
+
+    #     st.write("Data Preview")
+    #     st.write(df.drop("State-County", axis = 1))
 
 

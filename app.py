d85dde619c3c31e5d5af3ea934e036157ada85b6
Himanshu Goyal <hggoyal06@gmail.com>
2024-08-08 12:45:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,46 +140,57 @@ if uploaded_file is not None:
     # df = pd.read_excel(uploaded_file)
     df = read_excel(uploaded_file)
 
-    cols_required = ["Filing_County", "Filing_State"] 
-    cols_not_found = []
-    for col in cols_required:
-        if col not in df.columns:
-            cols_not_found.append(col)
+    df.to_excel("Test.xlsx", index = False)
+    with open("Test.xlsx", "rb") as zipped:
+        encoded_string = zipped.read()
+    
+    download = st.sidebar.download_button(label="Download Files", data = encoded_string, file_name= "Test.xlsx", mime='application/octet-stream')
 
-    if len(cols_not_found) > 0:
-        errors = {"Columns Not Found" : cols_not_found}
+    
+    st.write("Data Preview")
+    st.write(df)
 
-        st.error(json.dumps(errors, indent = 3)) 
+    
+    # cols_required = ["Filing_County", "Filing_State"] 
+    # cols_not_found = []
+    # for col in cols_required:
+    #     if col not in df.columns:
+    #         cols_not_found.append(col)
 
+    # if len(cols_not_found) > 0:
+    #     errors = {"Columns Not Found" : cols_not_found}
 
-    else:
-        # df["State-County"] = df["Filing_County"] + "-" + df["Filing_State"]
-
-        st.session_state.df = df
-        st.session_state.filters = list(df["State-County"].unique())
-
-
-        add_row = st.sidebar.button("Add Filter", on_click = add_filter)
-
-        i = 1
-
-        while i <= st.session_state.num_filter:
-
-            st.sidebar.multiselect(f"File {i}", options = st.session_state.filters, default = [], key = f"File-{i}")
-            st.sidebar.text_input("Random", value = f"File-{i}.xlsx", placeholder = f"File {i}.xlsx" , label_visibility="collapsed", key = f"Filename-{i}")
+    #     st.error(json.dumps(errors, indent = 3)) 
 
 
-            i = i + 1
+    # else:
+    #     # df["State-County"] = df["Filing_County"] + "-" + df["Filing_State"]
+
+    #     st.session_state.df = df
+    #     st.session_state.filters = list(df["State-County"].unique())
 
 
-        zipped_file = st.sidebar.button("Process Files", key='but_generate', on_click = generate_files)
+    #     add_row = st.sidebar.button("Add Filter", on_click = add_filter)
 
-        if zipped_file:
-            download = st.sidebar.download_button(label="Download Files", data = st.session_state.zipped, file_name= "Processed.zip", mime='application/octet-stream')
+    #     i = 1
+
+    #     while i <= st.session_state.num_filter:
+
+    #         st.sidebar.multiselect(f"File {i}", options = st.session_state.filters, default = [], key = f"File-{i}")
+    #         st.sidebar.text_input("Random", value = f"File-{i}.xlsx", placeholder = f"File {i}.xlsx" , label_visibility="collapsed", key = f"Filename-{i}")
+
+
+    #         i = i + 1
+
+
+    #     zipped_file = st.sidebar.button("Process Files", key='but_generate', on_click = generate_files)
+
+    #     if zipped_file:
+    #         download = st.sidebar.download_button(label="Download Files", data = st.session_state.zipped, file_name= "Processed.zip", mime='application/octet-stream')
 
         
-        st.write("Data Preview")
-        st.write(df)
+    #     st.write("Data Preview")
+    #     st.write(df)
         # st.write(df.drop("State-County", axis = 1))
 
 
